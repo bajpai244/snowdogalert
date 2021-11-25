a839fe733c16b6ca08a5bd451e1ddc4af6520a26
bajpai244 <bajpaiharsh244@gmail.com>
2021-11-25 09:09:58 +0530

    feat: add utility for getting current price against usd

---
+++ src/index.js
@@ -0,0 +1,4 @@
+// base price against which to compare, ex:-> 1200 means 1200 USDT
+const base_price = 1500;
+
+(async () => {})();
---
+++ src/lib/get_price.js
@@ -0,0 +1,21 @@
+const axios = require("axios");
+
+const config = {
+  method: "get",
+  url: "https://api.coingecko.com/api/v3/simple/price?ids=snowdog&vs_currencies=usd",
+  headers: {},
+};
+
+const get_data = async () => {
+  return await axios(config)
+    .then((response) => {
+      console.log(JSON.stringify(response.data));
+      return response.data.snowdog.usd;
+    })
+    .catch((error) => {
+      console.log(error);
+      return false;
+    });
+};
+
+module.exports = get_data;
